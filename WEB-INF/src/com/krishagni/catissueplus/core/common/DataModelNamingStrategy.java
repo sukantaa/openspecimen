d6326baade35a9f42558a130b6b95cb85b2ed245
Vinayak Pawar <vinayakapawar@gmail.com>
2017-01-10 07:16:59 +0530

    Got rid of import job hibernate mappings from OS source. The mappings will now be included from bulk-importer library during session factory creation.
    Added data model naming strategy to specify names used by OpenSpecimen
    for import jobs table and sequence.

--- WEB-INF/src/com/krishagni/catissueplus/core/common/DataModelNamingStrategy.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/common/DataModelNamingStrategy.java
@@ -8,7 +8,6 @@ import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 public class DataModelNamingStrategy extends PhysicalNamingStrategyStandardImpl implements PhysicalNamingStrategy {
 
 	public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-		System.err.println("*** TAB: " + name);
 		if (name.getText().equals("ImportJob")) {
 			return new Identifier("OS_BULK_IMPORT_JOBS", name.isQuoted());
 		} else if (name.getText().equals("ImportJob_params")) {
@@ -19,7 +18,10 @@ public class DataModelNamingStrategy extends PhysicalNamingStrategyStandardImpl 
 	}
 
 	public Identifier toPhysicalSequenceName(Identifier name, JdbcEnvironment context) {
-		System.err.println("*** SEQ: " + name);
-		return name;
+		if (name.getText().equals("ImportJobsSeq")) {
+			return new Identifier("OS_BULK_IMPORT_JOBS_SEQ", name.isQuoted());
+		} else {
+			return name;
+		}
 	}
 }
