278a3badb843dbfe50465ac524889c998188a4bb
Vinayak Pawar <vinayakapawar@gmail.com>
2017-01-26 08:29:30 +0530

    Auth utility cleanup.

--- WEB-INF/src/com/krishagni/catissueplus/core/common/util/AuthUtil.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/common/util/AuthUtil.java
@@ -1,45 +1,33 @@
 package com.krishagni.catissueplus.core.common.util;
 
-import java.net.URLDecoder;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.codec.Base64;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 
+import com.krishagni.auth.Util;
 import com.krishagni.catissueplus.core.administrative.domain.Institute;
 import com.krishagni.catissueplus.core.administrative.domain.User;
 
 public class AuthUtil {
-	private static final Log logger = LogFactory.getLog(AuthUtil.class);
-
 	public static Authentication getAuth() {
 		return SecurityContextHolder.getContext().getAuthentication();
 	}
 	
 	public static User getCurrentUser() {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (auth == null) {
-			return null;
-		}
-		
-		return (User)auth.getPrincipal();
+		return getAuth() == null ? null : (User) getAuth().getPrincipal();
 	}
 
 	public static Institute getCurrentUserInstitute() {
-		User user = AuthUtil.getCurrentUser();
+		User user = getCurrentUser();
 		return (user != null) ? user.getInstitute() : null;
 	}
 	
 	public static String getRemoteAddr() {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Authentication auth = getAuth();
 		if (auth == null) {
 			return null;
 		}
@@ -79,40 +67,14 @@ public class AuthUtil {
 	}
 	
 	public static String encodeToken(String token) {
-		return new String(Base64.encode(token.getBytes()));
+		return Util.encodeToken(token);
 	}
 	
 	public static String decodeToken(String token) {
-		return new String(Base64.decode(token.getBytes()));
+		return Util.decodeToken(token);
 	}
 	
 	public static String getTokenFromCookie(HttpServletRequest httpReq) {
-		String cookieHdr = httpReq.getHeader("Cookie");
-		if (StringUtils.isBlank(cookieHdr)) {
-			return null;
-		}
-
-		String[] cookies = cookieHdr.split(";");
-		String authToken = null;
-		for (String cookie : cookies) {
-			if (!cookie.trim().startsWith("osAuthToken")) {
-				continue;
-			}
-
-			String[] authTokenParts = cookie.trim().split("=");
-			if (authTokenParts.length == 2) {
-				try {
-					authToken = URLDecoder.decode(authTokenParts[1], "utf-8");
-					if (authToken.startsWith("%") || Utility.isQuoted(authToken)) {
-						authToken = authToken.substring(1, authToken.length() - 1);
-					}
-				} catch (Exception e) {
-					logger.error("Error obtaining token from cookie", e);
-				}
-				break;
-			}
-		}
-
-		return authToken;
+		return Util.getTokenFromCookie(httpReq, "osAuthToken");
 	}
 }
