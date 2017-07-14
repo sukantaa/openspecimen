469762f5aa6d6847708fe6c02a555bb76c6e782e
abhijit gosavi <abhi.gosavi12@gmail.com>
2017-07-14 11:43:43 +0530

    OPSMN-3816: Added institute filter on sites list page.

--- www/app/modules/administrative/site/list.js
+++ www/app/modules/administrative/site/list.js
@@ -1,16 +1,41 @@
 angular.module('os.administrative.site.list', ['os.administrative.models'])
-  .controller('SiteListCtrl', function($scope, $state, Site, Util, DeleteUtil, ListPagerOpts, CheckList) {
+  .controller('SiteListCtrl', function($scope, $state, currentUser,
+    Institute, Site, Util, DeleteUtil, ListPagerOpts, CheckList) {
 
     var pagerOpts;
+    var defInstitutes;
 
     function init() {
       pagerOpts = $scope.pagerOpts = new ListPagerOpts({listSizeGetter: getSitesCount});
       $scope.siteFilterOpts = {includeStats: true, maxResults: pagerOpts.recordsPerPage + 1};
       $scope.ctx = {
-        exportDetail: {objectType: 'site'}
+        exportDetail: {objectType: 'site'},
+        institutes: []
       };
+
+      loadInstitutes();
       loadSites($scope.siteFilterOpts);
       Util.filter($scope, 'siteFilterOpts', loadSites);
+    }
+
+    function loadInstitutes(searchString) {
+      if (!currentUser.admin) {
+        return;
+      }
+
+      if (defInstitutes && defInstitutes.length < 100) {
+        return;
+      }
+
+      Institute.query({name : searchString}).then(
+        function(institutes) {
+          $scope.ctx.institutes = institutes;
+
+          if (!searchString) {
+            defInstitutes = institutes;
+          }
+        }
+      );
     }
 
     function loadSites(filterOpts) {
@@ -30,6 +55,8 @@ angular.module('os.administrative.site.list', ['os.administrative.models'])
     function getSitesCount() {
       return Site.getCount($scope.siteFilterOpts);
     }
+
+    $scope.loadInstitutes = loadInstitutes;
 
     $scope.showSiteOverview = function(site) {
       $state.go('site-detail.overview', {siteId: site.id});
